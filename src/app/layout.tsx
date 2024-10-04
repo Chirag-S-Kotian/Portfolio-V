00de191f80a8830b3b775ca2c807debe9c71b07a
Chirag-S-kotian <ckotian117@gmail.com>
2024-10-04 13:39:30 +0530

    "Added @vercel/speed-insights dependency and imported SpeedInsights component in layout.tsx"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -65,6 +66,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="light">
           <TooltipProvider delayDuration={0}>
             {children}
+            <SpeedInsights />
             <Analytics />
             <Navbar />
           </TooltipProvider>
